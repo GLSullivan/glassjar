3c453fb969d60b34bc659ee78a9d1fb0c795f230
Greg Sullivan <66442873+GLSullivan@users.noreply.github.com>
2023-03-22 01:48:16 -0700

    Refining calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,38 +12,58 @@ const Calendar: React.FC<CalendarProps> = ({ onSelectDate }) => {
     return new Date(year, month + 1, 0).getDate();
   };
 
-  const generateDays = (month: number, year: number) => {
-    const days = [];
-    const totalDays = daysInMonth(month, year);
-
-    for (let day = 1; day <= totalDays; day++) {
-      days.push(new Date(year, month, day));
+  const [days, setDays] = useState<Date[]>(() => {
+    const firstDayOfMonth = new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1);
+    const firstDayOfGrid = new Date(firstDayOfMonth);
+    firstDayOfGrid.setDate(firstDayOfGrid.getDate() - firstDayOfGrid.getDay());
+  
+    const daysArray: Date[] = [];
+  
+    for (let i = 0; i < 42; i++) { // 42 days cover 6 weeks of the grid
+      daysArray.push(new Date(firstDayOfGrid.setDate(firstDayOfGrid.getDate() + 1)));
     }
-
-    return days;
+  
+    return daysArray;
+  });
+  
+  const isCurrentMonth = (day: Date) => {
+    return day.getMonth() === currentMonth.getMonth() && day.getFullYear() === currentMonth.getFullYear();
+  };
+  
+  const getMonthName = (date: Date) => {
+    const monthNames = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December',
+    ];
+    return `${monthNames[date.getMonth()]} ${date.getFullYear()}`;
   };
 
-  const days = generateDays(currentMonth.getMonth(), currentMonth.getFullYear());
 
+  
   return (
     <div className='container'>
       <div className='navigation'>
         <button onClick={() => setCurrentMonth(new Date(currentMonth.setMonth(currentMonth.getMonth() - 1)))}>
           Previous
         </button>
+        <span>{getMonthName(currentMonth)}</span>
         <button onClick={() => setCurrentMonth(new Date(currentMonth.setMonth(currentMonth.getMonth() + 1)))}>
           Next
         </button>
-      </div>
-      <div className='calendar'>
-        {days.map((day) => (
-          <button key={day.toISOString()} onClick={() => onSelectDate(day.toISOString().split('T')[0])} className='day'>
-            {day.getDate()}
-          </button>
-        ))}
-      </div>
+        </div>
+    <div className="calendar">
+      {days.map((day) => (
+        <button
+          key={day.toISOString()}
+          onClick={() => onSelectDate(day.toISOString().split('T')[0])}
+          className={`day ${!isCurrentMonth(day) ? 'other-month-day' : ''}`}
+        >
+          {day.getDate()}
+        </button>
+      ))}
     </div>
-  );
+  </div>
+);
 };
 
 export default Calendar;
