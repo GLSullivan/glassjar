bb8361477ee15543130b72f4dd57ae04d7e3eedf
Greg Sullivan <66442873+GLSullivan@users.noreply.github.com>
2023-03-22 23:52:54 -0700

    Additional changes to calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,69 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './../css/Calendar.css';
 
 interface CalendarProps {
-    onSelectDate: (date: string) => void;
-  }
+  onSelectDate: (date: string) => void;
+}
 
 const Calendar: React.FC<CalendarProps> = ({ onSelectDate }) => {
   const [currentMonth, setCurrentMonth] = useState(new Date());
 
-  const daysInMonth = (month: number, year: number) => {
-    return new Date(year, month + 1, 0).getDate();
-  };
-
-  const [days, setDays] = useState<Date[]>(() => {
-    const firstDayOfMonth = new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1);
+  const generateDaysArray = (month: Date) => {
+    const firstDayOfMonth = new Date(month.getFullYear(), month.getMonth(), 1);
     const firstDayOfGrid = new Date(firstDayOfMonth);
     firstDayOfGrid.setDate(firstDayOfGrid.getDate() - firstDayOfGrid.getDay());
-  
+
     const daysArray: Date[] = [];
-  
-    for (let i = 0; i < 42; i++) { // 42 days cover 6 weeks of the grid
+
+    for (let i = 0; i < 42; i++) {
       daysArray.push(new Date(firstDayOfGrid.setDate(firstDayOfGrid.getDate() + 1)));
     }
-  
+
     return daysArray;
-  });
-  
+  };
+
+  const [days, setDays] = useState<Date[]>(generateDaysArray(currentMonth));
+
+  useEffect(() => {
+    setDays(generateDaysArray(currentMonth));
+  }, [currentMonth]);
+
   const isCurrentMonth = (day: Date) => {
     return day.getMonth() === currentMonth.getMonth() && day.getFullYear() === currentMonth.getFullYear();
   };
+
+  const weeksVisibility = (month: Date) => {
+    const firstDayOfMonth = new Date(month.getFullYear(), month.getMonth(), 1);
+    const lastDayOfMonth = new Date(month.getFullYear(), month.getMonth() + 1, 0);
+    const firstDayOfWeek = firstDayOfMonth.getDay();
+    const lastDayOfWeek = lastDayOfMonth.getDay();
+    const totalDays = lastDayOfMonth.getDate() + firstDayOfWeek + (lastDayOfWeek !== 6 ? (6 - lastDayOfWeek) : 0);
+    const totalWeeks = Math.ceil(totalDays / 7);
+    return Array.from({ length: 6 }, (_, i) => i < totalWeeks);
+  };
   
-  const getMonthName = (date: Date) => {
-    const monthNames = [
-      'January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December',
-    ];
-    return `${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+  const weekVisibility = weeksVisibility(currentMonth);
+  
+  const isToday = (date: Date) => {
+    const today = new Date();
+    return (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    );
   };
 
-
-  
   return (
-    <div className='container'>
-      <div className='navigation'>
+    <div className='calendar__container'>
+      <div className='calendar__navigation'>
         <button onClick={() => setCurrentMonth(new Date(currentMonth.setMonth(currentMonth.getMonth() - 1)))}>
           Previous
         </button>
-        <span>{getMonthName(currentMonth)}</span>
+        <h2 className="calendar__month" onClick={() => setCurrentMonth(new Date())}>
+          {currentMonth.toLocaleString('default', { month: 'long' })} {currentMonth.getFullYear()}
+        </h2>
         <button onClick={() => setCurrentMonth(new Date(currentMonth.setMonth(currentMonth.getMonth() + 1)))}>
           Next
         </button>
-        </div>
-    <div className="calendar">
-      {days.map((day) => (
-        <button
-          key={day.toISOString()}
-          onClick={() => onSelectDate(day.toISOString().split('T')[0])}
-          className={`day ${!isCurrentMonth(day) ? 'other-month-day' : ''}`}
-        >
-          {day.getDate()}
-        </button>
-      ))}
+      </div>
+      <div className="calendar__calendar">
+        {days.map((day, index) => (
+          <button
+            key={day.toISOString()}
+            onClick={() => onSelectDate(day.toISOString().split('T')[0])}
+            className={`calendar__day${!isCurrentMonth(day) ? ' calendar__day--other-month' : ''
+              }${isToday(day) ? ' calendar__day--today' : ''}`}
+            style={{ visibility: weekVisibility[Math.floor(index / 7)] ? 'visible' : 'hidden' }}
+          >
+            {day.getDate()}
+          </button>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Calendar;
