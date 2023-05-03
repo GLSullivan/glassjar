6cab3cd224bc563022ba4efc231a198aa5093aeb
Greg Sullivan <66442873+GLSullivan@users.noreply.github.com>
2023-05-02 23:07:48 -0700

    Transaction Helper

--- src/components/forms/MiniTransactionForm.tsx
+++ src/components/forms/MiniTransactionForm.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import CurrencyInput from 'react-currency-input-field';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CurrencyInput from "react-currency-input-field";
 
-import { addTransaction } from '../../redux/slices/transactions';
-import { RootState } from '../../redux/store';
-import { Transaction } from '../../models/Transaction';
+import { addTransaction } from "../../redux/slices/transactions";
+import { RootState } from "../../redux/store";
+import { Transaction } from "../../models/Transaction";
 
-// import './../../css/MiniForms.css';
+import "./../../css/MiniForms.css";
 
 interface MiniTransactionFormProps {
   initialName?: string;
@@ -14,9 +14,13 @@ interface MiniTransactionFormProps {
   initialDay?: string;
 }
 
-const MiniTransactionForm: React.FC<MiniTransactionFormProps> = ({ initialName = '', initialAmount = 0, initialDay = '' }) => {
+const MiniTransactionForm: React.FC<MiniTransactionFormProps> = ({
+  initialName = "",
+  initialAmount = 0,
+  initialDay = "",
+}) => {
   const accounts = useSelector((state: RootState) => state.accounts.accounts);
-  const firstAccountId = accounts.length > 0 ? accounts[0].id : '';
+  const firstAccountId = accounts.length > 0 ? accounts[0].id : "";
 
   const [transactionName, setTransactionName] = useState(initialName);
   const [day, setDay] = useState(initialDay);
@@ -28,7 +32,11 @@ const MiniTransactionForm: React.FC<MiniTransactionFormProps> = ({ initialName =
     e.preventDefault();
 
     const currentDate = new Date();
-    const nextTransactionDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), parseInt(day));
+    const nextTransactionDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      parseInt(day)
+    );
 
     if (nextTransactionDate <= currentDate) {
       nextTransactionDate.setMonth(nextTransactionDate.getMonth() + 1);
@@ -39,51 +47,95 @@ const MiniTransactionForm: React.FC<MiniTransactionFormProps> = ({ initialName =
     const transactionData: Transaction = {
       transactionName,
       date: isoDate,
-      type: 'withdrawal',
+      type: "withdrawal",
       amount,
-      description: '',
+      description: "",
       fromAccount: firstAccountId,
-      toAccount: '',
+      toAccount: "",
       id: new Date().getTime(),
       isRecurring: true,
-      endDate: '',
-      recurrenceFrequency: 'monthly',
+      endDate: "",
+      recurrenceFrequency: "monthly",
       allowOverpayment: false,
-      showInCalendar: true
+      showInCalendar: true,
     };
 
     dispatch(addTransaction(transactionData));
 
-    setTransactionName('');
-    setDay('');
+    setTransactionName("");
+    setDay("");
     setAmount(0);
   };
 
   return (
-    <form className="mini-transaction-form" onSubmit={handleSubmit}>
-      <input
-        placeholder="Name"
-        type="text"
-        value={transactionName}
-        onChange={(e) => setTransactionName(e.target.value)}
-      />
-      <input
-        placeholder="Day"
-        type="number"
-        min="1"
-        max="31"
-        value={day}
-        onChange={(e) => setDay(e.target.value)}
-      />
-      <CurrencyInput
-        prefix="$"
-        name="amount"
-        placeholder="Amount"
-        defaultValue={amount / 100}
-        decimalsLimit={2}
-        onValueChange={(value) => setAmount(value ? Math.round(parseFloat(value) * 100) : 0)}
-      />
-      <button type="submit">Add</button>
+    <form className="glassjar__mini-transaction" onSubmit={handleSubmit}>
+      <div className="glassjar__form__input-group">
+        {/* <label htmlFor="Name">Transaction Name:</label> */}
+        <input
+          placeholder="Name"
+          type="text"
+          value={transactionName}
+          onChange={(e) => setTransactionName(e.target.value)}
+        />
+      </div>
+
+      <div className="glassjar__mini-transaction__sub">
+        <div className="glassjar__form__input-group glassjar__form__input-group--drop">
+          <label htmlFor="due">Due On The:</label>
+          <select id="due" onChange={(e) => setDay(e.target.value)}>
+            <option value="1">1st</option>
+            <option value="2">2nd</option>
+            <option value="3">3rd</option>
+            <option value="4">4th</option>
+            <option value="5">5th</option>
+            <option value="6">6th</option>
+            <option value="7">7th</option>
+            <option value="8">8th</option>
+            <option value="9">9th</option>
+            <option value="10">10th</option>
+            <option value="11">11th</option>
+            <option value="12">12th</option>
+            <option value="13">13th</option>
+            <option value="14">14th</option>
+            <option value="15">15th</option>
+            <option value="16">16th</option>
+            <option value="17">17th</option>
+            <option value="18">18th</option>
+            <option value="19">19th</option>
+            <option value="20">20th</option>
+            <option value="21">21st</option>
+            <option value="22">22nd</option>
+            <option value="23">23rd</option>
+            <option value="24">24th</option>
+            <option value="25">25th</option>
+            <option value="26">26th</option>
+            <option value="27">27th</option>
+            <option value="28">28th</option>
+            <option value="29">29th</option>
+            <option value="30">30th</option>
+            <option value="31">31st</option>
+          </select>
+        </div>
+
+        <div className="glassjar__form__input-group">
+          <label htmlFor="amount">Amount:</label>
+          <CurrencyInput
+            prefix="$"
+            id="amount"
+            name="amount"
+            placeholder="Amount"
+            defaultValue={amount / 100}
+            decimalsLimit={2}
+            onValueChange={(value) =>
+              setAmount(value ? Math.round(parseFloat(value) * 100) : 0)
+            }
+          />
+        </div>
+
+        <button type="submit">
+          <i className="fa-solid fa-plus" />
+        </button>
+      </div>
     </form>
   );
 };
--- src/components/helpers/TransactionHelper.tsx
+++ src/components/helpers/TransactionHelper.tsx
@@ -9,30 +9,29 @@ import { recurringExpenses } from "../../data/RecurringExpenses";
 
 const Loader: React.FC = () => {
 
-  return ( 
-    <div>
+  return (
+    <>
       <h2>Let's get you started.</h2>
-      <p>Enter your recurring transactions. Not sure about something? Just guess!</p>
-      <p>If you don't see something here, adding it later is easy. No worries.</p>
+      <p>
+        Enter your recurring transactions. Not sure about something? Just guess!
+      </p>
+      <p>
+        If you don't see something here, adding it later is easy. No worries.
+      </p>
 
       {recurringExpenses.map((categoryItem, index) => (
-<>
-
-
-
-        <div key={index} className="category">
-          <h3>{categoryItem.category}</h3>
-          <ul>
-            {categoryItem.expenses.map((expense, expenseIndex) => (
-<MiniTransactionForm key={expenseIndex} initialName={expense} initialDay="1" />
-              // <li key={expenseIndex}>{expense}</li>
+        <>
+          <div key={index}>
+            <h3>{categoryItem.category}</h3>
+            <div>
+              {categoryItem.expenses.map((expense, expenseIndex) => (
+                <MiniTransactionForm key={expenseIndex} initialName={expense} initialDay="1" />
               ))}
-          </ul>
-        </div>
-              </>
+            </div>
+          </div>
+        </>
       ))}
-
-    </div>
+    </>
   );
 };
 
